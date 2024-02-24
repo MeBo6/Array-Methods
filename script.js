90785b0d661ca3a1ef812211f27a7090f494acf6
MeBo6 <gmerabishvili1236@gmail.com>
2024-02-24 16:36:40 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// 1. array 1 dan 5mde. gadavaqciot stringad ori gzit. 
+// 2. shevabrunot masivi(reverse)
+// 3. map it davabrunt yvela elementi.
+// 4. 3ze meti elementi davabrunot filter it.
+// 5. masivshi chavamatot da amovshalot pirveli elementi
+// 6. masivshi chavamatot da amovshalot bolo elementi
+
+
+
+const myArr = [1,2,3,4,5];
+
+// 1a.
+// const fun = (myArr) => {
+//    console.log(myArr.join(','));
+// }
+// fun(myArr);
+
+// 1b.
+// const fun = (myArr) => {
+//    console.log(myArr.toString(','));
+// }
+// fun(myArr);
+
+
+// 2.
+// console.log(myArr.reverse());
+
+// 3.
+// const newArr = myArr.map(e => console.log(e));
+
+
+// 4.
+// const filteredArr = myArr.filter(function filterFun(e) {return e > 3});
+// const filteredArr = myArr.filter(e => e > 3);
+// console.log(filteredArr);
+
+
+// 5a.
+// myArr.shift(); console.log(myArr);
+
+// 5b.
+// myArr.unshift(0); console.log(myArr);
+
+// 6a.
+// myArr.pop(); console.log(myArr);
+
+// 6b.
+// myArr.push(6); console.log(myArr);
+
+
+
+
+
+
+
+
+
